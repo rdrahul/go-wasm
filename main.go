1dd95cfbaa277cdb9749e8a0d24bb67b4c56104b
Rahul Dwivedi <rahuldwivedi@Rahuls-MacBook-Air.local>
2019-11-13 15:46:54 +0530

    dom operations

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"syscall/js"
+)
 
 func main() {
-	fmt.Println("Hello wasm")
+
+	//getting the document object
+	document := js.Global().Get("document")
+
+	//create a p tag by calling createElement
+	p := document.Call("createElement", "p")
+
+	//set the innerHtml attribute for that p
+	p.Set("innerHTML", "Hello WASM from Go!")
+
+	//append the created p tag to our document
+	document.Get("body").Call("appendChild", p)
 }
