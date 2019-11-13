caad3c45271a449a64cdc16553bd44be9b7bcc08
Rahul Dwivedi <rahuldwivedi@Rahuls-MacBook-Air.local>
2019-11-13 16:10:00 +0530

    successfully call

--- functioncall.go
+++ functioncall.go
@@ -11,9 +11,16 @@ func printMessage(this js.Value, inputs []js.Value) interface{} {
 	p := document.Call("createElement", "p")
 	p.Set("innerHTML", message)
 	document.Get("body").Call("appendChild", p)
+	return nil
 
 }
 
 func main() {
+	//created a channel
+	c := make(chan bool)
+
 	js.Global().Set("printMessage", js.FuncOf(printMessage))
+
+	//listening for it
+	<-c
 }
